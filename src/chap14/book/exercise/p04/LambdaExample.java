1c0caeca6f0d107efa7bf9bfc99d87c6579e2bde
kbm <99157638+byeongminkim0@users.noreply.github.com>
2022-03-25 16:26:46 +0900

    java (lambda)

---
+++ src/chap14/book/exercise/p04/LambdaExample.java
@@ -0,0 +1,18 @@
+package chap14.book.exercise.p04;
+
+import java.util.function.IntSupplier;
+
+public class LambdaExample {
+	public static int method(int x, int y) {
+		IntSupplier supplier = () -> {
+//			x *= 10;
+			int result = x + y;
+			return result;
+		};
+		int result = supplier.getAsInt();
+		return result;
+	}
+	public static void main(String[] args) {
+		System.out.println(method(3,5));
+	}
+}
